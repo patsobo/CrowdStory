826378ecb9c585c1110ff23d88991811c03b64b0
Patrick Sobolewski <Patrick Sobolewski>
2016-06-02 14:44:36 -0600

    Display a cleaner date for the archives.

--- client/templates/story/story-item.js
+++ client/templates/story/story-item.js
@@ -1,4 +1,14 @@
 Template.storyItem.helpers({
+	/**
+     * Returns a list of all posts available to the current user.
+     */
+	'displayDate': function(date) {
+		var monthNames = ["January", "February", "March", "April", "May", "June",
+		  "July", "August", "September", "October", "November", "December"
+		];
+
+		return monthNames[date.getMonth()] + " " + date.getDate() + ", "  + date.getFullYear();
+	},
 });
 
 Template.storyItem.events({
